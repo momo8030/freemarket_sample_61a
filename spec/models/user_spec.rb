6c1bef169f32ecf634838d1b8fc8c33e0799f24c
asuka1216 <56636325+asuka1216@users.noreply.github.com>
2020-01-26 17:11:34 +0900

    complete2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 describe User do
   describe '#create' do
+    it "is valid with a email" do
+      user = User.new(nickname: "aaa", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+    end
     it "is invalid without a email" do
-     user = User.new(nickname: "aaa", email: "", password: "00000000", password_confirmation: "00000000")
-     user.valid?
-     expect(user.errors[:email]).to include("can't be blank")
+      user = User.new(nickname: "aaa", email: "", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
     end
   end
 end
