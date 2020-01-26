240f9f5810797656ebb04ca9ceeaf7b38dd1c69f
momo8030 <momo5938@icloud.com>
2020-01-26 16:04:28 +0900

    users_table

--- db/migrate/20200125082531_devise_create_users.rb
+++ db/migrate/20200125082531_devise_create_users.rb
@@ -11,6 +11,15 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.integer :birthday, null: false
       t.string :comment
       t.integer :phone_number, null: false, unique: true
+
+      t.string :encrypted_password, null: false, default: ""
+      
+      t.string :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      t.datetime :remember_created_at
+
+      t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
