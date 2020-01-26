4d23bcef62c40cf350aa2b0969d400e820500fe2
asuka1216 <56636325+asuka1216@users.noreply.github.com>
2020-01-26 16:40:24 +0900

    complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ApplicationRecord
   has_many :likes
   has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
   has_many :selling_items, foreign_key: "seller_id", class_name: "Item"
+
+  validates :email, presence: true
 end
