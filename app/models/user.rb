26e486c12ddb3593aad3add8fcc33617b1f00553
asuka1216 <56636325+asuka1216@users.noreply.github.com>
2020-02-01 17:18:26 +0900

    complite

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ApplicationRecord
   has_one :creditcard
   has_many :comments
   has_many :likes
-  has_many :buyed_items, foreign_key: "buyer_id", class_name: "Item"
+  has_many :todos
+  has_many :bought_items, foreign_key: "buyer_id", class_name: "Item"
   has_many :selling_items, foreign_key: "seller_id", class_name: "Item"
 
   validates :email, presence: true
