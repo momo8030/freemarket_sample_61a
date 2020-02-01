b6b2bfdb49d43ced7dddd8f663e13549b5ca6df7
asuka1216 <56636325+asuka1216@users.noreply.github.com>
2020-02-01 17:41:32 +0900

    a

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,11 @@ ActiveRecord::Schema.define(version: 2020_01_25_082531) do
     t.string "password", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
-    t.integer "birthday", null: false
+    t.string "first_name_kana", null: false
+    t.string "last_name_kana", null: false
+    t.date "birth_year", null: false
+    t.date "birth_month", null: false
+    t.date "birth_day", null: false
     t.string "comment"
     t.integer "phone_number", null: false
     t.string "encrypted_password", default: "", null: false
