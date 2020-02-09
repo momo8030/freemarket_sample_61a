883e33c4c0c9450ef1ce48f4dc28267b34f09a60
ask1208 <t.ask.128@icloud.com>
2020-02-09 17:00:20 +0900

    signups_contlloer作成

---
+++ app/controllers/signups_controller.rb
@@ -0,0 +1,16 @@
+class SignupsController < ApplicationController
+
+  def user1
+    @user = User.new
+  end
+
+  def user2
+    @user = User.new
+  end
+
+ 
+  def new
+    
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,11 @@ Rails.application.routes.draw do
     get :profile_edit
   end
   
+  resources :signups, only: [:new, :create] do
+    collection do
+      get 'user1'
+      get 'user2'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
