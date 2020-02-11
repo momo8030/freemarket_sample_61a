7310dd3ca58481ceab4ad3354e39cf05631a0566
ask1208 <t.ask.128@icloud.com>
2020-02-11 20:15:57 +0900

    微調整

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :signups, only: [:new, :create] do
     collection do
       get 'user1'
-      get 'user2'
+      post 'user2'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
