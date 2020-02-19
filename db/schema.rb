a66478073a329cc97ca51ee7e4ab174dad5f4a68
momo8030 <momo5938@icloud.com>
2020-02-19 20:56:55 +0900

    test2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_121326) do
+ActiveRecord::Schema.define(version: 2020_02_19_112024) do
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "provider"
@@ -24,18 +24,15 @@ ActiveRecord::Schema.define(version: 2020_02_05_121326) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", null: false
-    t.string "password", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
-
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
     t.integer "birth_year", null: false
     t.integer "birth_month", null: false
     t.integer "birth_day", null: false
-
     t.string "comment"
-    t.integer "phone_number", null: false
+    t.string "phone_number", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
