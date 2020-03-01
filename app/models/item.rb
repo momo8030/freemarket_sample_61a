5d6474a4ca3a1b710706a55717de07c68741db8b
Asihda488 <akgno.12612@gmail.com>
2020-03-01 20:58:36 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,10 @@ class Item < ApplicationRecord
   belongs_to_active_hash :delivery_method
   # belongs_to user, foreign_key: 'user_id'
   belongs_to :category
+  belongs_to :seller, class_name: "User"
+  belongs_to :buyer, class_name: "User"
+  has_many   :images,dependent: :destroy
+  # accepts_nested_attributes_for :images,allow_destory: true
+  has_many :likes
+
 end
