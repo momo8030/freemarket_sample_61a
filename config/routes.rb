c379ac208a5f746b6d34e312a471385b03966d27
momo8030 <momo5938@icloud.com>
2020-03-02 00:39:18 +0900

    update_signout

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   omniauth_callbacks: 'users/omniauth_callbacks',
   registrations: 'users/registrations'
   }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   
   root 'items#index'
   resources :items, only: [:index, :new, :show] do
