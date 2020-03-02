ed6e3755d2f0e1919598f144fc55c38bdd63b1c8
Asihda488 <akgno.12612@gmail.com>
2020-03-02 20:17:37 +0900

    fix-image.rb

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,6 +3,5 @@ class Image < ApplicationRecord
   # 画像アップロード時に必要な記述
   # mount_uploders :url,ImageUploder 
 
-  validates :url, presence: true
-  validates :item_id, presence: true
+  validates :url,:item_id, presence: true
 end
