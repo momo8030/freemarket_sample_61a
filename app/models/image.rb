8c067b4f0e24a44e6df4579e99a50787309616a3
asuka1216 <56636325+asuka1216@users.noreply.github.com>
2020-03-04 21:24:59 +0900

    aaaa

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ApplicationRecord
-  belongs_to :item
-  # 画像アップロード時に必要な記述
-  # mount_uploders :url,ImageUploder 
+    # 画像アップロード時に必要な記述
+  mount_uploaders :url,ImageUploader
+  belongs_to :item 
 
   validates :url,:item_id, presence: true
 end
