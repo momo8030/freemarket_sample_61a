bc4d5b9efc2d4c0638c0df91674fc564dfc7752a
asuka1216 <56636325+asuka1216@users.noreply.github.com>
2020-03-04 21:42:44 +0900

    aaaa

--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :cards,only:[:new, :create,:show]do
+  resources :cards,only:[:new, :create,:show] do
    
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
