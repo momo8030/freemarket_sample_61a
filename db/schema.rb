dcfa649fc28cbc89b85f07f6120be05418dfaf6b
momo8030 <momo5938@icloud.com>
2020-03-08 16:13:34 +0900

    brand-delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_01_100146) do
+ActiveRecord::Schema.define(version: 2020_03_08_064232) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
     t.string "city"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
