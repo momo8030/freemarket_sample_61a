90a7f96460050e0b8b57e3a2c7b80595e8219d9f
momo8030 <momo5938@icloud.com>
2020-03-12 14:39:54 +0900

    items_update

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ApplicationRecord
   belongs_to :item
   # 画像アップロード時に必要な記述
-  mount_uploaders :url,ImageUploader 
+  mount_uploader :url, ImageUploader 
 
-  validates :url,:item_id, presence: true
+  validates :url, presence: true
 end
