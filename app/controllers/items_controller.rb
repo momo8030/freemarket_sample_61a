106a9c74b3cac8cc2af21074f64de1177713c7da
Asihda488 <akgno.12612@gmail.com>
2020-03-14 14:33:53 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,31 @@
 class ItemsController < ApplicationController
+  before_action :set_item,only: [:show,:show_mypage]
   def index
   end
 
   def show
+     if user_signed_in?
+       if @item.seller_id == current_user.id
+         redirect_to show_mypage_items_path
+       else
+         show_item
+       end
+     else
+       show_item
+    end
+  end
+
+  def set_item
     @item = Item.find(params[:id])
+  end
+  def show_mypage
+    show_item
+  end
+  def show_item
     @images = @item.images.order('id ASC')
     @seller = @item.seller
     @brand = @item.brand
+    @seller_items = Item.where(seller_id: @seller.id).order("id DESC").limit(6)
   end
 
   def new
