f9d586da7b30202f8e75c147ffc128d471e5e880
Asihda488 <akgno.12612@gmail.com>
2020-03-22 14:53:18 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,6 @@ Rails.application.routes.draw do
       post 'show', to: 'cards#show'
     end
   end
-
+    
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
